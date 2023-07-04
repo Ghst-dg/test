68159fbb0c4aeda56fc443eac9f182f07a09e5c1
Priyanshu Verma <64607887+Ghst-dg@users.noreply.github.com>
2023-07-04 20:45:27 +0530

    Created program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void generateSeries(int x) {
+    int currentNumber = 1;
+    cout << "Output: ";
+    for (int i = 1; i <= x; i++) {
+        cout << currentNumber << " ";
+        currentNumber += 2;
+    }
+    cout << endl;
+}
+
+int main() {
+    int x;
+
+    cout << "Enter the value of x: ";
+    cin >> x;
+
+    generateSeries(x);
+
+    return 0;
+}
